8bdac49e67c137d8dd73058bfba1e5d0238a2b7b
Ming Yue <mingyue312@gmail.com>
2018-04-29 18:57:31 -0400

    need to add the two new jar into instructor/Libraries, pie chart display code added

--- instructor/WEB-INF/src/mypkg/ShowQuestionResults.java
+++ instructor/WEB-INF/src/mypkg/ShowQuestionResults.java
@@ -1,11 +1,19 @@
-
 package mypkg;
 
 import java.io.*;
+import java.awt.BasicStroke;
+import java.awt.Color;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.util.*;
 import java.sql.*;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.general.DefaultPieDataset;
+import org.jfree.chart.labels.StandardPieSectionLabelGenerator;
+import org.jfree.chart.plot.PiePlot;
 
 
 public class ShowQuestionResults extends HttpServlet{
@@ -15,7 +23,8 @@ public class ShowQuestionResults extends HttpServlet{
     private static final String DB_URL = "jdbc:mysql://localhost:3306/";
     private static final String DB_NAME = "cs5999";
     
-    private Dictionary pollResult = new Hashtable();
+    //private Dictionary pollResult = new Hashtable();
+    private Map<String, Integer> pollStats = new HashMap<String, Integer>();
     
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -27,18 +36,21 @@ public class ShowQuestionResults extends HttpServlet{
         String username = (String) httpSession.getAttribute("username");
         String sessionID = (String) httpSession.getAttribute("sessionID");
         
-        PrintWriter out = response.getWriter();
         int success = 0; //1 if all notmal, -1 if operation failed
         
         if (actionType.equals("View Results")){
-            success = this.viewResults(sessionID, questionID, response);
-            if (success == -1)
+            success = this.viewResults(sessionID, questionID, request, response);
+            if (success == -1){
+                PrintWriter out = response.getWriter();
                 out.println("Failed to view result for "+ questionID);
+            }
         }
         else if (actionType.equals("Download Results")){
+            PrintWriter out = response.getWriter();
             success = this.downloadResults(sessionID, questionID);
-            if (success == -1)
+            if (success == -1){
                 out.println("Failed to download result.");
+            }
             else
                 out.println("CSV File is created successfully.");
         }
@@ -53,12 +65,12 @@ public class ShowQuestionResults extends HttpServlet{
         doGet(request, response);
     }
     
-    private int viewResults (String sessionID, String questionID, HttpServletResponse response)
+    private int viewResults (String sessionID, String questionID, HttpServletRequest request, HttpServletResponse response)
             throws IOException, ServletException {
         //connect to db, set table correspondingto current session to writable
         Connection conn = null;
         PreparedStatement stmt;
-        PrintWriter out = response.getWriter();
+        //PrintWriter out = response.getWriter();
         try {
             Class.forName("com.mysql.jdbc.Driver");
             conn = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
@@ -78,14 +90,35 @@ public class ShowQuestionResults extends HttpServlet{
 
             // iterate through the java resultset
             while (rs.next()) {
-                String studentID = rs.getString("studentID");
+                //String studentID = rs.getString("studentID");
                 String ans = rs.getString("ans");
                 // print the results
-                pollResult.put(studentID, ans);
-                out.println("<p>"+studentID+":"+ans+"</p>");
+                //pollResult.put(studentID, ans);
+                //out.println("<p>"+studentID+":"+ans+"</p>");
+                
+                if (pollStats.get(ans)==null){
+                    pollStats.put(ans, 0);
+                }
+                pollStats.put(ans, pollStats.get(ans)+1);
             }
-            return 1;
 
+            OutputStream streamOut = response.getOutputStream();
+            DefaultPieDataset myServletPieChart = new DefaultPieDataset();
+            
+            for ( String key : pollStats.keySet() ) {
+                myServletPieChart.setValue(key, pollStats.get(key));
+            }
+       
+            JFreeChart mychart = ChartFactory.createPieChart(questionID + " Poll Result",myServletPieChart,true,true,false);
+            PiePlot ColorConfigurator = (PiePlot) mychart.getPlot();
+            ColorConfigurator.setLabelGenerator(new StandardPieSectionLabelGenerator("{0}:{1}"));
+            ColorConfigurator.setLabelBackgroundPaint(new Color(220, 220, 220));  
+            response.setContentType("image/png"); /* Set the HTTP Response Type */
+            /* Send a big chart back to the browser */
+            ChartUtilities.writeChartAsPNG(streamOut, mychart, 640, 480);/* Write the data to the output stream */
+            
+            pollStats.clear();
+            return 1;
         } catch (Exception e) {
             e.printStackTrace();
             return -1;
